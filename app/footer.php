571add1e9c70e5a3f739d3ea3ecb529ae55f6bcd
Reza Hadiwijaya Dynasti <ppg067@unm.ac.id>
2023-07-21 11:15:15 +0800

    fix active menu

--- app/footer.php
+++ app/footer.php
@@ -55,7 +55,18 @@
         });
     });
 </script>
-
+<script>
+//    menu actiov saat di klik
+    $(document).ready(function () {
+        var url = window.location;
+        $('ul.nav-sidebar a').filter(function () {
+            return this.href == url;
+        }).addClass('active');
+        $('ul.nav-treeview a').filter(function () {
+            return this.href == url;
+        }).parentsUntil(".nav-sidebar > .nav-treeview").addClass('menu-open').prev('a').addClass('active');
+    });
+  </script>
 <?php
 if (isset($_GET['status'])) {
     if ($_GET['status'] == 'sukses') {
